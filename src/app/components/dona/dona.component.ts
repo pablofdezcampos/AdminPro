97ea89a3b8352f6da00bd18343fdd912c676c607
Pablo <pablofdezcvc@hotmail.com>
2022-09-20 11:29:51 +0200

    Fix Error of label values

--- src/app/components/dona/dona.component.ts
+++ src/app/components/dona/dona.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 
@@ -9,6 +9,22 @@ import { ChartData, ChartEvent, ChartType } from 'chart.js';
   ]
 })
 export class DonaComponent {
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.doughnutChartData = {
+      labels: this.doughnutChartLabels,
+      datasets: [
+        {
+          data: [350, 450, 100],
+          backgroundColor: ['#6857E6', '#009FEE', '#F02059'],
+          hoverBackgroundColor: ['#0000FF','#0000FF','#0000FF'],
+          hoverBorderColor:['#000000','#000000','#000000']
+        }
+      ]
+    }
+  }
+
+
 
   @Input() title: string = '';
 
