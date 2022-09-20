990ae3249d55b4a21ed770d01fff4f996cc23731
Pablo <pablofdezcvc@hotmail.com>
2022-09-20 10:08:27 +0200

    Adding dona grahic with external libraries

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-graphic1',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Graphic1Component implements OnInit {
 
-  constructor() { }
+export class Graphic1Component {
 
-  ngOnInit(): void {
-  }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [ 350, 450, 100 ],
+        backgroundColor: ['#6857E6', '#009FEE', '#F02059'],
+        hoverBackgroundColor: ['#0000FF','#0000FF','#0000FF'],
+        hoverBorderColor:['#000000','#000000','#000000']
+      }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
 }
